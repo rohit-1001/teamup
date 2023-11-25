ce33f975a6de6c2cc97d257f4523bd57fb5ca9af
rohit <rohitphalke2003@gmail.com>
2023-11-25 23:03:30 +0530

    Added padding to not found page

--- client/src/components/NotFound.jsx
+++ client/src/components/NotFound.jsx
@@ -5,7 +5,9 @@ const NotFound = () => {
   return (
     <div style={{margin: "0 auto",
       width: "900px",
-      height: "auto"}}>
+      height: "auto",
+      paddingTop: "120px"
+      }}>
     <img style={{
       maxWidth: "100%",
       height: "auto"
